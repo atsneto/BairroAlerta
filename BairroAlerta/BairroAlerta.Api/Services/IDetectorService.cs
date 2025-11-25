14ad581d6cfa7251cc94e7e3789a3b6507a86ae3
Tosca <142675701+atsneto@users.noreply.github.com>
2025-11-24 21:19:04 -0300

    Add comment to GerarAlertaFake method

--- BairroAlerta/BairroAlerta.Api/Services/IDetectorService.cs
+++ BairroAlerta/BairroAlerta.Api/Services/IDetectorService.cs
@@ -2,8 +2,9 @@ using BairroAlerta.Api.Models;
 
 namespace BairroAlerta.Api.Services
 {
+    // Interface do serviço de detecção
     public interface IDetectorService
     {
-        Alerta GerarAlertaFake();
+        Alerta GerarAlertaFake(); // Método para criar alerta falso
     }
 }
